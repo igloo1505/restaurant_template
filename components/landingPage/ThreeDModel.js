b86f917d96739391a3b9eb7382a925bdd690030d
Andrew Iglinski <Andrew@igloodevelopment.dev>
2021-10-15 18:45:35 -0500

    Texture as mtl file being added successfully

--- components/landingPage/ThreeDModel.js
+++ components/landingPage/ThreeDModel.js
@@ -8,44 +8,56 @@ import OBJLoader from './ObjLoader';
 import { Object3D } from "three/src/core/Object3D"; //Object3D types
 import { AnimationClip } from "three/src/animation/AnimationClip"; //Animati
 import { useLoader } from '@react-three/fiber'
+import {MTLLoader} from "three/examples/jsm/loaders/MTLLoader"
 
 
 
 
 const Model = () => {
     const [model, setModel] = useState(false)    
+    const [texture, setTexture] = useState(false)
     const [animationState, setAnimationState] = useState(null)
 
     const group = useRef()
     useEffect(() => {
         // const loader = new GLTFLoader();
-        const loader = new OBJLoader();
-        loader.load("/assets/coffeeCup.obj", (cup) => {
-            console.log('cup: lp', cup);
-            // const nodes = obj
-            setModel(cup)
-        }, 
-        // onProgress,
-        //  onError
-         )
-        // loader.load("scene.gltf", async (gltf) => {
-        //   const nodes = await gltf.parser.getDependencies("node");
-        //   const animations = await gltf.parser.getDependencies("animation");
-        //   setModel(nodes[0]);
-        //   setAnimationState(animations);
-        // });
-      }, []);
+        // const loader = new OBJLoader();
+        // loader.load("/assets/coffeeCup.obj", (cup) => {
+        //     setModel(cup)
+        // }
+        // )
+        // const mtlLoader = new MTLLoader();
+        // mtlLoader.load('/assets/coffeeCupTexture.mtl', (_t) => {
+        //     _t.preload()
+        //     console.log('_t: cup: ', _t);
+        //     // setTexture(_t)
+        // })
+        var mtlLoader = new MTLLoader();
+            // mtlLoader.setPath('obj/male02/');
+            mtlLoader.load('/assets/coffeeCupTexture.mtl', function(materials) {
+              materials.preload();
+              var objLoader = new OBJLoader();
+              objLoader.setMaterials(materials);
+            //   objLoader.setPath('obj/male02/');
+              objLoader.load('/assets/coffeeCup.obj', function(object) {
+                // object.position.y = -95;
+                // scene.add(object);
+                setModel(object)
+              });
+            });
+
+    }, []);
 
       const {progress} = useProgress();
     //   const cupTexture = useLoader(TextureLoader, '/assets/uploads_files_830234_coffe.mtl')
-    // <meshStandardMaterial map={cupTexture}/>
-
+    
     return (
         <>
         {model ? (
             <group ref={group} position={[-20, 0, 0]} dispose={null}>
             <mesh>
             <primitive ref={group} name="Object_0" object={model} />
+            <meshStandardMaterial map={texture}/>
             </mesh>
           </group>
         ) : 
