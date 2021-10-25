9e91ac712c5a937d04125afc309c3340bcddb3ec
Andrew Iglinski <Andrew@igloodevelopment.dev>
2021-10-24 22:37:52 -0500

    hopefully fixed deployment issue

--- next.config.js
+++ next.config.js
@@ -26,6 +26,20 @@ module.exports = withPWA(
 		images: {
 			domains: ["localhost"],
 		},
+		env: {
+			MONGO_URI: process.env.MONGO_URI,
+			JWT_SECRET: process.env.JWT_SECRET,
+			MONGODB_DB: process.env.MONGODB_DB,
+			AWS_BUCKET_NAME: process.env.AWS_BUCKET_NAME,
+			AWS_BUCKET_REGION: process.env.AWS_BUCKET_REGION,
+			AWS_ACCESS_KEY: process.env.AWS_ACCESS_KEY,
+			AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+			EDAMAM_RECIPE_APP_ID: process.env.EDAMAM_RECIPE_APP_ID,
+
+			EDAMAM_RECIPE_ACCESS_KEY: process.env.EDAMAM_RECIPE_ACCESS_KEY,
+			EDAMAM_FOOD_APP_ID: process.env.EDAMAM_FOOD_APP_ID,
+			EDAMAM_FOOD_ACCESS_KEY: process.env.EDAMAM_FOOD_ACCESS_KEY,
+		},
 		serverRuntimeConfig: {
 			MONGO_URI: process.env.MONGO_URI,
 			JWT_SECRET: process.env.JWT_SECRET,
